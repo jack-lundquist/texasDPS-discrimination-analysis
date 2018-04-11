ba1997398527e55f35ff3de3d0a3942f5af2f599
jack-lundquist <31962755+jack-lundquist@users.noreply.github.com>
2018-04-11 14:28:47 -0400

    updating formatting

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
 //Initialize tooltips
-$(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip();
-});
+// $(document).ready(function(){
+//     $('[data-toggle="tooltip"]').tooltip();
+// });
+
+//Popup intro modal on load
+$('#useModal').modal('show');
 
 //Create the map variable
 var map = L.map('my-map', {
     scrollWheelZoom: false
-}).setView([31.512996, -99.217529], 5);
+}).setView([31.512996, -99.217529], 6);
 
 //Add the basemap
 L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
@@ -14,3 +17,37 @@ L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x
 	subdomains: 'abcd',
 	maxZoom: 19
 }).addTo(map);
+
+
+var map1 = L.map('my-map1', {
+    scrollWheelZoom: false
+}).setView([31.512996, -99.217529], 6);
+
+//Add the basemap
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+	subdomains: 'abcd',
+	maxZoom: 19
+}).addTo(map1);
+
+var map2 = L.map('my-map2', {
+    scrollWheelZoom: false
+}).setView([31.512996, -99.217529], 6);
+
+//Add the basemap
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+	subdomains: 'abcd',
+	maxZoom: 19
+}).addTo(map2);
+
+var map3 = L.map('my-map3', {
+    scrollWheelZoom: false
+}).setView([31.512996, -99.217529], 6);
+
+//Add the basemap
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+	subdomains: 'abcd',
+	maxZoom: 19
+}).addTo(map3);
